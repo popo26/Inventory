4458819e7d76995b67e9bc8ef29fb291d81892e7
popo26 <mynameispopo26@gmail.com>
2024-01-05 09:19:01 +1300

    updated swagger

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 require("dotenv").config();
-var cors = require("cors");
-var app = express();
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+swaggerDocument = require("./swagger.json");
+const app = express();
 
 app.use(cors());
 
 // parse requests of content-type -application / json;
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.get("/", (req, res) => {
   res.json({
-    message: "Welcome to my Exercise4 (Backend with MVC) application.",
+    message: "Welcome to Inventory application.",
   });
 });
 
@@ -18,11 +22,9 @@ let productRoutes = require("./routes/productRoutes");
 let customerRoutes = require("./routes/customerRoutes");
 let orderRoutes = require("./routes/orderRoutes");
 
-
 app.use("/api/products", productRoutes);
 app.use("/api/customers", customerRoutes);
 app.use("/api/orders", orderRoutes);
-
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8082;
